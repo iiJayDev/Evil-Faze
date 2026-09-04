0317b91fed56e0ffd69124d19675c7f3008db283
iiJayDev <iijayplayzyt9@gmail.com>
2026-09-04 09:25:08 -0600

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "Faze tung SUCKS",     // this is what people actually see
+        state: "POLO SUCKS!Don't listen to him",     // this is what people actually see
         type: 4,               // Custom
       },
     ],
